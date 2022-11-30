e5594ebfaaa4079bb48cf7c42920f5cc793d15c4
Tad McCorkle <tad.mccorkle@gmail.com>
2022-11-29 23:40:47 -0500

    initial commit

---
+++ 2022/src/days/day01.rs
@@ -0,0 +1,7 @@
+pub fn part1(input: &str) -> String {
+    String::from("solution not yet implemented")
+}
+
+pub fn part2(input: &str) -> String {
+    String::from("solution not yet implemented")
+}
