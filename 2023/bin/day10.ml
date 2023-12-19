668abc4e27a28b38ffc4b207a7f956a8b89349d2
Tad McCorkle <tad.mccorkle@gmail.com>
2023-12-18 22:12:49 -0500

    wip: solution to 2023 day 10 pt 1

--- 2023/bin/day10.ml
+++ 2023/bin/day10.ml
@@ -1,21 +1,8 @@
 open StdLabels
 
-let input = Aoc.read_input 10
-
-(* | is a vertical pipe connecting north and south. *)
-(* - is a horizontal pipe connecting east and west. *)
-(* L is a 90-degree bend connecting north and east. *)
-(* J is a 90-degree bend connecting north and west. *)
-(* 7 is a 90-degree bend connecting south and west. *)
-(* F is a 90-degree bend connecting south and east. *)
-(* . is ground; there is no pipe in this tile. *)
-(* S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has. *)
-
 type point = { x : int; y : int }
-
-type pipe = N of char | E of char | S of char | W of char
-
-let string_of_point p = string_of_int p.x ^ "," ^ string_of_int p.y
+type direction = N | E | S | W
+type pipe = U of char | R of char | D of char | L of char
 
 let find_start lines =
   let rec fs lines row =
@@ -23,20 +10,73 @@ let find_start lines =
     | [] -> Aoc.raise_input_failure ()
     | hd :: rest -> (
         match String.index_opt hd 'S' with
-        | Some s -> { x = row; y = s }
+        | Some s -> { x = s; y = row }
         | None -> fs rest (row + 1))
   in
   fs lines 0
 
-let connects = function
-  | N p -> p = '|' || p = '7' || p = 'F'
-  | _ -> failwith "TODO"
+let input = Aoc.read_input 10
+let maze = Array.of_list input
+let start = find_start input
 
-let traverse = ()
+let get_char pt = String.get (Array.get maze pt.y) pt.x
+
+let start_connects pt dir =
+  match get_char pt with
+  | '|' -> dir = N || dir = S
+  | '-' -> dir = E || dir = W
+  | 'L' -> dir = S || dir = W
+  | 'J' -> dir = E || dir = S
+  | '7' -> dir = N || dir = E
+  | 'F' -> dir = N || dir = W
+  | _ -> Aoc.raise_input_failure ()
+
+let rec traverse start curr_pipe curr_pt step =
+  if start = curr_pt then step
+  else
+    let next_pt, dir =
+      match curr_pipe with
+      | U '|'
+      | R 'J'
+      | L 'L' ->
+          ({ curr_pt with y = curr_pt.y - 1 }, N)
+      | U 'F'
+      | R '-'
+      | D 'L' ->
+          ({ curr_pt with x = curr_pt.x + 1 }, E)
+      | R '7'
+      | D '|'
+      | L 'F' ->
+          ({ curr_pt with y = curr_pt.y + 1 }, S)
+      | U '7'
+      | D 'J'
+      | L '-' ->
+          ({ curr_pt with x = curr_pt.x - 1 }, W)
+      | _ -> Aoc.raise_input_failure ()
+    in
+    let next_char = get_char next_pt in
+    let next_pipe =
+      match dir with
+      | N -> U next_char
+      | E -> R next_char
+      | S -> D next_char
+      | W -> L next_char
+    in
+    traverse start next_pipe next_pt (step + 1)
 
 let () =
-  let rows = List.length input in
-  let cols = List.hd input |> String.length in
-  let _ = Array.make rows (Array.make cols false) in
-  let start = find_start input in
-  print_endline @@ string_of_point start
+  let rows = Array.length maze in
+  let cols = String.length (List.hd input) in
+  let north = { start with y = start.y - 1 } in
+  let east = { start with x = start.x + 1 } in
+  let south = { start with y = start.y + 1 } in
+  let west = { start with x = start.x - 1 } in
+  let next_pipe, next_pt =
+    if north.y >= 0 && start_connects north N then (U (get_char north), north)
+    else if east.x < cols && start_connects east E then (R (get_char east), east)
+    else if south.y < rows && start_connects south S then (D (get_char south), south)
+    else if west.x >= 0 && start_connects west W then (L (get_char west), west)
+    else Aoc.raise_input_failure ()
+  in
+  let steps = traverse start next_pipe next_pt 1 in
+  print_endline @@ string_of_int (steps / 2)
