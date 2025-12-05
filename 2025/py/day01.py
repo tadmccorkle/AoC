8c097005a05e80723aea4ab26e9b2cfd30029b04
Tad McCorkle <tad.mccorkle@gmail.com>
2025-12-04 23:36:16 -0500

    py: 2025 day 1 p2

--- 2025/py/day01.py
+++ 2025/py/day01.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 
 # with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../input/sample.txt"), "r") as f:
@@ -7,15 +8,39 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../input/day
 
 # p1
 
+start = time.perf_counter()
 zeroes = 0
 point = 50
 for rot in rots:
     diff = 100 - int(rot[1:]) if rot[0] == 'L' else int(rot[1:])
     point = ((point + diff) % 100)
-    if point == 0: 
+    if point == 0:
         zeroes += 1
-print(zeroes)
+end = time.perf_counter()
+
+print(f'P1: {zeroes} (time: {end - start:.6f} s)')
 
 # p2
 
-print("P2: TODO")
+start = time.perf_counter()
+zeroes = 0
+point = 50
+for rot in rots:
+    diff = int(rot[1:])
+    if rot[0] == 'L':
+        p = point - diff
+        if p == 0 and point > 0:
+            zeroes += 1
+        elif p < 0:
+            if point > 0:
+                zeroes += 1
+            zeroes += abs(p) // 100
+            p = (p + 100) % 100
+        point = p
+    else:
+        point += diff
+        zeroes += point // 100
+        point %= 100
+end = time.perf_counter()
+
+print(f'P2: {zeroes} (time: {end - start:.6f} s)')
